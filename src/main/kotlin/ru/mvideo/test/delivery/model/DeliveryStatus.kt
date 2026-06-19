dd7fd6d218a8fe34ea6408740a3e93d88c8b438e
phohlova <hohlova.pv@edu.spbstu.ru>
2026-06-19 12:56:35 +0300

    feat: добавлена валидация переходов с помощью метода canTransitionTo()

--- src/main/kotlin/ru/mvideo/test/delivery/model/DeliveryStatus.kt
+++ src/main/kotlin/ru/mvideo/test/delivery/model/DeliveryStatus.kt
@@ -1,6 +1,19 @@
 package ru.mvideo.test.delivery.model
 
 enum class DeliveryStatus {
-    CREATED, ASSIGNED, IN_TRANSIT, DELIVERED, CANCELLED
+    CREATED, ASSIGNED, IN_TRANSIT, DELIVERED, CANCELLED;
+
+    fun canTransitionTo(next: DeliveryStatus): Boolean {
+        if (this == next) {
+            return false
+        }
+
+        return when (this) {
+            CREATED -> next == ASSIGNED || next == IN_TRANSIT || next == CANCELLED
+            ASSIGNED -> next == IN_TRANSIT || next == CANCELLED
+            IN_TRANSIT -> next == DELIVERED
+            DELIVERED, CANCELLED -> false
+        }
+    }
 }
 
